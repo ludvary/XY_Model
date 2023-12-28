32763eb85476419df418cfd4902ba12da290e9c2
ludvary <rskrishnadasnair@gmail.com>
2023-12-28 11:05:34 +0530

    add lattice.jl

--- lattice.jl
+++ lattice.jl
@@ -3,13 +3,14 @@ module LatticeJl
 
     mutable struct Lattice
         L :: Int64
-        spins :: Matrix{Vector{Float64}}
+        spins :: Matrix{Vector{Float64}}        # make a matrix of spins
 
         function Lattice(L::Int64)
-            new(L, [normalize([1,1]-2*rand(2)) for _ in 1:L, _ in 1:L])
+            new(L, [normalize([1,1]-2*rand(2)) for _ in 1:L, _ in 1:L])     # spins are randomly oriented and are normalized
         end
     end
 
+    # return an array containing the four nearest neighbours 
     function get_neighbours(i::Int64, j::Int64, lattice::Lattice)
         
         neighbours = [
